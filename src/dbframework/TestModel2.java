1473cd2b14d53b345e49010eb863aad2cb62f245
AuliaYF <auliayf@gmail.com>
2015-02-10 17:03:39 +0700

    Added TestModel2

---
+++ src/dbframework/TestModel2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbframework;
+
+import i.project.core.Model;
+
+/**
+ *
+ * @author auliayf
+ */
+public class TestModel2 extends Model {
+
+    public TestModel2() {
+        this.TABLE_NAME = "Kompetensi_keahlian";
+        this.PRIMARY_KEY = "Kode_KK";
+    }
+}
